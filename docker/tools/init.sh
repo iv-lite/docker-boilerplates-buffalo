45415c207e452fcdaa7465bd5efafcd16673166d
iv-lite <ibekwe.victor@gmail.com>
2024-06-09 04:35:27 +0200

    Add - flags support

--- docker/tools/init.sh
+++ docker/tools/init.sh
@@ -5,15 +5,22 @@ if [ -z $1 ]; then
     exit 1;
 fi
 
+name=$1
+flags=""
+while [ -n $2 ]; do
+    flags+=" $2"
+    shift
+done
+
 if [ ! -f /.dockerenv ]; then
     docker compose build;
-    docker compose run app init.sh $1 --rm ;
+    docker compose run app init.sh $name "${flags}" --rm ;
     exit;
 fi
 
 # inside docker where in <root>:/app
 if [ ! -d src ] && [ -z DEBUG ]; then
-    buffalo new $1 --vcs none --skip-docker;
+    buffalo new $1 --vcs none --skip-docker "${flags};
     mv $1 src;
     rm -rf .git;
     git init;
